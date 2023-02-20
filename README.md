71acdb76f0efcde1ef144bc34412c6941ce1fe75
gs7joao <gsjoaopedro7@gmail.com>
2023-02-20 13:49:34 -0300

    Estruturando API RESTful e realizando testes

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 
 ## Laravel
 - Projeto para estudos criando e consumindo API de estudantes utilizando Laravel com documentação da Twilio.
+
+```bash
+ TITLE                  METHOD     URL                                   PARAMS
+
+ Create Students        POST       http://localhost:8000/api/students    {name, course}
+ Get All Students       GET        http://localhost:8000/api/students
+ Get Only Student       GET        http://localhost:8000/api/students/{id}
+ Update Student         PUT        http://localhost:8000/api/students/{id}
+ Delete Student         DELETE     http://localhost:8000/api/students/{id}
+```
+
+## Tecnologias
+
+[![My Skills](https://skills.thijs.gg/icons?i=laravel,postman)](https://skills.thijs.gg)
+
+
+## Contato
+
+:email: gsjoaopedro7@gmail.com
+
+:telephone_receiver: (19) 99744-9537
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ApiController; //Importa controller para estanciar a function
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('students', 'ApiController@getAllStudents'); //Versão Lavaral 7 <= 
+Route::get('students', [ApiController::class,'getAllStudents']); // Versao Laravel 8 >= 
+Route::get('students/{id}', [ApiController::class, 'getStudent']);
+Route::post('students', [ApiController::class,'createStudent']);
+Route::put('students/{id}', [ApiController::class,'updateStudent']);
+Route::delete('students/{id}', [ApiController::class,'deleteStudent']);
